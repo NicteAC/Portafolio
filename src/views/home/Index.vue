0d70e7d697c9998158b210b78c1d065b03a82b83
Nicte <carola.aliaga@gmail.com>
2021-06-26 22:38:15 -0400

    mejorando vistas

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container id="home" class="pt-12 pb-0 px-0" fluid tag="section">
+    <v-img src="./05.png" >
     <headline />
+    </v-img>
   </v-container>
 </template>
 
--- src/views/home/components/Headline.vue
+++ src/views/home/components/Headline.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="intro" class="text-center mb-12">
     <v-responsive class="mx-auto subtitle-1 grey--text">
-      <v-img src="./05B.png" max-height="500" max-width="500"></v-img>
+      
     </v-responsive>
   </section>
 </template>
